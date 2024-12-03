874f960b8c9a278f6347186cf34b597ab4def96e
Fkaique <kaique.918273@gmail.com>
2024-12-03 20:12:46 -0300

    neh

--- beecrowd/1025.c
+++ beecrowd/1025.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 #include <math.h>
-
-int obterDigito(int numero, int posicao) {
-    int divisor = pow(10,posicao);
-    return (numero / divisor) % 10;
+#define MAX 65
+void swap(int* a,int* b){
+    int c = *a;
+    *a=*b;
+    *b=c;
+}
+void ordena(vet){/////errado
+    for (i=n-1; i>0; i--);{
+        for (int j=0; j<i;j++){
+            if (vet[i]<vet[j]){
+                swap(vet[i],vet[j]);
+            }
+        }
+    }
 }
 int main() {
-    int numero = 95;
-    int posicao = 1; // Posição do dígito (0 = unidade, 1 = dezena, etc.)
-    printf("Dígito na posição %d: %d\n", posicao, obterDigito(numero, posicao));
-    return 0;
+    int i, j;
+    int n, q;
+    scanf("%d",&n);
+    scanf("%d",&q);
+    int vet[n], consulta[q];
+    for (int i=0; i<n && i<MAX; i++){
+        scanf("%d",&vet[i]);
+    }
+    
+
+    for (int i=0; i<n; i++){
+        printf("%d ",vet[i]);
+    }
+
 }
